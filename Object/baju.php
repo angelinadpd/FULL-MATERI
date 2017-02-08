48250411646b97a5adfd1af129855b756212deb4
Mrs. Angel <angelinadpd@gmail.com>
2017-02-08 09:28:54 +0700

    add tambah

---
+++ Object/baju.php
@@ -0,0 +1,19 @@
+
+<?php
+    class baju
+    {
+        public $kain;
+        public $benang;
+        public $kain;
+
+        public function dipakai()
+        {
+            return 'baju dipakai';
+        }
+
+        public function disimpan()
+        {
+            return 'baju disimpan';
+        }
+    }
+?>
---
+++ Object/saklar.php
@@ -0,0 +1,18 @@
+ <?php
+    class saklar
+    {
+        public $induk;
+        public $anak;
+        public $baut;
+
+        public function hidup()
+        {
+            return 'lampu hidup';
+        }
+
+        public function mati()
+        {
+            return 'lampu mati';
+        }
+    }
+?>
