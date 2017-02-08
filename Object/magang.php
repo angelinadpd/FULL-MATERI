b3e605b888e5585f76e091f7f0067018250233c3
Mrs. Angel <angelinadpd@gmail.com>
2017-02-08 09:42:16 +0700

    add tukar

---
+++ Object/magang.php
@@ -0,0 +1,17 @@
+ <?php
+    class magang
+    {
+        public $smk;
+        public $kuliah;
+
+        public function materi()
+        {
+            return 'pelajari dan review materi';
+        }
+
+        public function tugas()
+        {
+            return 'bikin tugas';
+        }
+    }
+?>
